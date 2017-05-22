86460d433b3f41b7c1caf77e6505ee8071779cdf
Len512 <echatzak@gmail.com>
2017-05-22 14:33:51 +0200

    Update and rename Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p034 - BeerSong/main.c to Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p036 - BeerSong/main.c

--- Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p036 - BeerSong/main.c
+++ Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p036 - BeerSong/main.c
@@ -5,20 +5,20 @@
 
 #include <stdio.h>
 
-void singTheSong (int numberOfBottles){
+void singSongFor (int numberOfBottles){
 
     if (numberOfBottles == 0){
-        printf("These are simply no more bottles of beer on the wall.\n");
+        printf("These are simply no more bottles of beer on the wall.\n\n");
     } else {
         printf("%d bottles of beer on the wall. %d bottles of beer.\n", numberOfBottles, numberOfBottles);
         int oneFewer = numberOfBottles - 1;
-        printf("Take one down, pass it around, %d bottles of beer on the wall.\n", oneFewer);
-        singTheSong(oneFewer); // This function calls itself!
+        printf("Take one down, pass it around, %d bottles of beer on the wall.\n\n", oneFewer);
+        singSongFor(oneFewer); // This function calls itself!
         printf("Put a bottle in the recycling, %d empty bottles in the bin.\n", numberOfBottles);
     }
 }
 int main(int argc, const char * argv[]) {
-
-    singTheSong(99);
+    // We could sing 99 verses but 4 is easier to think about
+    singSongFor(4);
     return 0;
 }
