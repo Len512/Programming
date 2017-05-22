e630d04dcbf2a163344cac3a9acc4a3ab0d6399d
Len512 <echatzak@gmail.com>
2017-05-22 12:18:50 +0200

    Create main.c

---
+++ Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 06/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    
+    double angle = 1;
+    double sine = sin(angle);
+    printf("The sine of %f radian is %.3f\n", angle, sine);
+    return 0;
+}
