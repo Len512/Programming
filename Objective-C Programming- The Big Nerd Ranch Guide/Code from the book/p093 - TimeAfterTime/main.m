1500cb4fd7a8d8ae7fa2726d7ef94bfe7499e40a
Len512 <echatzak@gmail.com>
2017-05-22 15:51:29 +0200

    Update and rename Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p079 - TimeAfterTime/main.m to Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p093 - TimeAfterTime/main.m

--- Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p093 - TimeAfterTime/main.m
+++ Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p093 - TimeAfterTime/main.m
@@ -12,8 +12,6 @@ int main(int argc, const char * argv[]) {
         NSLog(@"The date is %@", now);
         double seconds = [now timeIntervalSince1970];
         NSLog(@"It has been %f seconds since the start of 1970.", seconds);
-        NSDate *later = [now dateByAddingTimeInterval:100000];
-        NSLog(@"In 100,000 seconds it will be %@", later);
     }
     return 0;
 }
