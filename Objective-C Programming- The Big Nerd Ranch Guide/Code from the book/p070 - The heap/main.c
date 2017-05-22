8711b99f1dba30dd4c10453dcb8b2222a1a2e9e1
Len512 <echatzak@gmail.com>
2017-05-22 13:51:11 +0200

    Create main.c

---
+++ Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p070 - The heap/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    float heightInMeters;
+    int weightInKilos;
+} Person;
+
+float bodyMassIndex (Person *p){
+    return p->weightInKilos / (p->heightInMeters * p->heightInMeters);
+}
+int main(int argc, const char * argv[]) {
+    
+    // Allocate memory for one Person structure
+    Person *x = (Person *) malloc(sizeof(Person));
+    
+    // Fill in two members of the structure
+    x->heightInMeters = 2.0;
+    x->weightInKilos = 81;
+    
+    // Print out the BMI of the original Person
+    float xBMI = bodyMassIndex(x);
+    printf("x has a BMI of %f\n", xBMI);
+    
+    // Let the memory be recycled
+    free(x);
+    
+    // Forget where it was
+    x = NULL;
+    
+    return 0;
+}
