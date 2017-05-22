98fb789c78a5dd1fafed6fc6a58385b2871c6ec4
Len512 <echatzak@gmail.com>
2017-05-22 14:41:23 +0200

    Create main.c

---
+++ Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 06 - Squarer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Squarer
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int i = 5;
+    
+    printf("\"%i\" squared is \"%i\".\n", i, i*i);
+    
+    return 0;
+}
