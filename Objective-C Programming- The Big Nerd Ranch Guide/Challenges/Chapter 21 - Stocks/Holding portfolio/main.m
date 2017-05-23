65825d40f238c5f2427ec84a301be891399472d3
Len512 <echatzak@gmail.com>
2017-05-23 13:05:03 +0200

    Update main.m

--- Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 21 - Stocks/Holding portfolio/main.m
+++ Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 21 - Stocks/Holding portfolio/main.m
@@ -18,21 +18,24 @@ int main(int argc, const char * argv[]) {
         [stock1 setPurchaseSharePrice: 2.30];
         [stock1 setCurrentSharePrice: 4.50];
         [stock1 setNumberOfShares: 40];
+        [stock1 setStockTicker:@"XYZ"];
         [portfolio addStockholding:stock1];
         
         [stock2 setPurchaseSharePrice: 12.19];
         [stock2 setCurrentSharePrice: 10.55];
         [stock2 setNumberOfShares: 90];
+        [stock2 setStockTicker:@"ABC"];
         [portfolio addStockholding:stock2];
         
         [stock3 setPurchaseSharePrice: 45.10];
         [stock3 setCurrentSharePrice: 49.51];
         [stock3 setNumberOfShares: 210];
         [stock3 setConversionRate: 0.94];
+        [stock3 setStockTicker:@"LMN"];
         [portfolio addStockholding:stock3];
         
         for (BNRStockHolding *s in portfolio.stockHoldings){
-            NSLog(@"Value of stock: %.02f", [s valueInDollars]);
+            NSLog(@"%@", s);
         }
         NSLog(@"Value of portfolio: %.02f", [portfolio valueInDollars]);
         
