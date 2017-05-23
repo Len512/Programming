0cec417f58b5cdbacb5fef96a7a9ac6dbf651a14
Len512 <echatzak@gmail.com>
2017-05-23 12:39:39 +0200

    Create BNRAsset.h

---
+++ Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p158 - BMITime/BNRAsset.h
@@ -0,0 +1,13 @@
+//
+//  BNRAsset.h
+//  BMITime
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRAsset : NSObject
+
+@property (nonatomic, copy) NSString *label;
+@property (nonatomic) unsigned int resaleValue;
+
+@end
