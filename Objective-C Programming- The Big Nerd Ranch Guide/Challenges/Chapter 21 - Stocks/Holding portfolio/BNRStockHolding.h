230b3c9286f1890ee732eac01ebd9e29f75b98b5
Len512 <echatzak@gmail.com>
2017-05-23 13:04:15 +0200

    Update BNRStockHolding.h

--- Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 21 - Stocks/Holding portfolio/BNRStockHolding.h
+++ Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 21 - Stocks/Holding portfolio/BNRStockHolding.h
@@ -9,6 +9,7 @@
 @property (nonatomic) float purchaseSharePrice;
 @property (nonatomic) float currentSharePrice;
 @property (nonatomic) int numberOfShares;
+@property (nonatomic, copy) NSString *stockTicker;
 
 - (float) costInDollars;
 - (float) valueInDollars;
