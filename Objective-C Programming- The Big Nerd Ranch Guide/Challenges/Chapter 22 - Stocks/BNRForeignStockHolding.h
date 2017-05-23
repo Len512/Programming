935b970048d98f9ffa555a50e36410e07665d044
Len512 <echatzak@gmail.com>
2017-05-23 13:40:44 +0200

    Create BNRForeignStockHolding.h

---
+++ Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 22 - Stocks/BNRForeignStockHolding.h
@@ -0,0 +1,10 @@
+//
+//  BNRForeignStockHolding.h
+//  Stocks
+//
+
+#import "BNRStockHolding.h"
+
+@interface BNRForeignStockHolding : BNRStockHolding
+@property (nonatomic) float conversionRate;
+@end
