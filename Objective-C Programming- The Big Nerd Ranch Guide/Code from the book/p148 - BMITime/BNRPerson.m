0e393ca77bc9dc532536d6438fc18f9f38647555
Len512 <echatzak@gmail.com>
2017-05-23 11:40:50 +0200

    Create BNRPerson.m

---
+++ Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p148 - BMITime/BNRPerson.m
@@ -0,0 +1,15 @@
+//
+//  BNRPerson.m
+//  BMITime
+//
+
+#import "BNRPerson.h"
+
+@implementation BNRPerson
+
+- (float) bodyMassIndex {
+    float h = [self heightInMeters];
+    return [self weightInKilos] / (h * h);
+}
+
+@end
