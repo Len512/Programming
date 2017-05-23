43d2d8053ebf9c25852ebfc8789c554132e57bbe
Len512 <echatzak@gmail.com>
2017-05-23 13:41:12 +0200

    Create BNRPortfolio.h

---
+++ Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 22 - Stocks/BNRPortfolio.h
@@ -0,0 +1,16 @@
+//
+//  BNRPortfolio.h
+//  Stocks
+//
+
+#import <Foundation/Foundation.h>
+
+@class BNRStockHolding;
+
+@interface BNRPortfolio : NSObject
+
+- (void) addStockholding: (BNRStockHolding *) s;
+- (void) removeStockHolding: (BNRStockHolding *) s;
+- (float) valueInDollars;
+
+@end
