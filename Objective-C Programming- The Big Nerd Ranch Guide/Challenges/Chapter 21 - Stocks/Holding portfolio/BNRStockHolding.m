1a5032f8a2a39328dec5af1364d8df47a9f6dc08
Len512 <echatzak@gmail.com>
2017-05-23 13:04:36 +0200

    Update BNRStockHolding.m

--- Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 21 - Stocks/Holding portfolio/BNRStockHolding.m
+++ Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 21 - Stocks/Holding portfolio/BNRStockHolding.m
@@ -13,5 +13,7 @@
 - (float) valueInDollars {
     return _currentSharePrice * _numberOfShares;
 }
-
+- (NSString *) description {
+    return [NSString stringWithFormat:@"Stock holding %@ has a total value of %.2f", self.stockTicker, [self valueInDollars]];
+}
 @end
