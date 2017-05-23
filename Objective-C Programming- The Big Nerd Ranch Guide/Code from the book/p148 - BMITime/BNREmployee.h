16dc4f510090b80891f9255c67a261a01ffc7965
Len512 <echatzak@gmail.com>
2017-05-23 11:41:08 +0200

    Create BNREmployee.h

---
+++ Objective-C Programming- The Big Nerd Ranch Guide/Code from the book/p148 - BMITime/BNREmployee.h
@@ -0,0 +1,14 @@
+//
+//  BNREmployee.h
+//  BMITime
+//
+
+#import <Foundation/Foundation.h>
+#import "BNRPerson.h"
+
+@interface BNREmployee : BNRPerson
+@property (nonatomic) unsigned int employeeID;
+@property (nonatomic) unsigned int officeAlarmCode;
+@property (nonatomic) NSDate *hireDate;
+- (double) yearsOfEmployment;
+@end
