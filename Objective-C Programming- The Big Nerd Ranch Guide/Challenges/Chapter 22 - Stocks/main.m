9d0567bee05922764c3a781a5203abad2470e683
Len512 <echatzak@gmail.com>
2017-05-23 13:40:01 +0200

    Create main.m

---
+++ Objective-C Programming- The Big Nerd Ranch Guide/Challenges/Chapter 22 - Stocks/main.m
@@ -0,0 +1,44 @@
+//
+//  main.m
+//  Stocks
+//
+
+#import <Foundation/Foundation.h>
+#import "BNRForeignStockHolding.h"
+#import "BNRPortfolio.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        BNRPortfolio *portfolio = [[BNRPortfolio alloc] init];
+        BNRStockHolding *stock1 = [[BNRStockHolding alloc] init];
+        BNRStockHolding *stock2 = [[BNRStockHolding alloc] init];
+        BNRForeignStockHolding *stock3 = [[BNRForeignStockHolding alloc] init];
+        
+        [stock1 setPurchaseSharePrice: 2.30];
+        [stock1 setCurrentSharePrice: 4.50];
+        [stock1 setNumberOfShares: 40];
+        [stock1 setStockTicker:@"XYZ"];
+        [portfolio addStockholding:stock1];
+        
+        [stock2 setPurchaseSharePrice: 12.19];
+        [stock2 setCurrentSharePrice: 10.55];
+        [stock2 setNumberOfShares: 90];
+        [stock2 setStockTicker:@"ABC"];
+        [portfolio addStockholding:stock2];
+        
+        [stock3 setPurchaseSharePrice: 45.10];
+        [stock3 setCurrentSharePrice: 49.51];
+        [stock3 setNumberOfShares: 210];
+        [stock3 setConversionRate: 0.94];
+        [stock3 setStockTicker:@"LMN"];
+        [portfolio addStockholding:stock3];
+        
+        NSLog(@"Value of portfolio: %.02f", [portfolio valueInDollars]);
+        
+        [portfolio removeStockHolding:stock3];
+        NSLog(@"Value of portfolio: %.02f", [portfolio valueInDollars]);
+        
+    }
+    return 0;
+}
