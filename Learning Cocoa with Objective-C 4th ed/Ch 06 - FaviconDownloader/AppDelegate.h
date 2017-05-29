147a70a95a4423ca220084f21894ba55b6add250
Len512 <echatzak@gmail.com>
2017-05-29 15:53:39 +0200

    Create AppDelegate.h

---
+++ Learning Cocoa with Objective-C 4th ed/Ch 06 - FaviconDownloader/AppDelegate.h
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.h
+//  FaviconsDownloader
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
