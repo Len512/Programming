fb860af8ad441d88cc2ef819c791d42a1a2a68d3
Len512 <echatzak@gmail.com>
2017-05-29 15:54:13 +0200

    Create ViewController.h

---
+++ Learning Cocoa with Objective-C 4th ed/Ch 06 - FaviconDownloader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FaviconsDownloader
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
