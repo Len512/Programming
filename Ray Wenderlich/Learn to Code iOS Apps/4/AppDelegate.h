10d7b44288aef6613e7cd4d5093f99bab8da9a37
Len512 <echatzak@gmail.com>
2017-05-30 14:15:30 +0200

    Create AppDelegate.h

---
+++ Ray Wenderlich/Learn to Code iOS Apps/4/AppDelegate.h
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.h
+//  Tap Me
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
