6bee42d04f6fe500bc0b9f414a486a4b64290196
Len512 <echatzak@gmail.com>
2017-05-30 17:04:48 +0200

    Create Truck.h

---
+++ Ray Wenderlich/Intro to Object-Oriented Design/1/Truck.h
@@ -0,0 +1,15 @@
+//
+//  Truck.h
+//  Vehicles
+//
+//  Created by Len512 on 30/05/2017.
+//  Copyright © 2017 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Truck : Vehicle
+
+@property (nonatomic, assign) NSInteger cargoCapacityCubicFeet;
+
+@end
