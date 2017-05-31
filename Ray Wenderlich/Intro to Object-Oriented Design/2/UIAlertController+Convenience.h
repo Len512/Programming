38cf505c11e5a884a9d89fab38f0204c4c1a6791
Len512 <echatzak@gmail.com>
2017-05-31 12:28:58 +0200

    Create UIAlertController+Convenience.h

---
+++ Ray Wenderlich/Intro to Object-Oriented Design/2/UIAlertController+Convenience.h
@@ -0,0 +1,15 @@
+//
+//  UIAlertController+Convenience.h
+//  Vehicles
+//
+//  Created by Len512 on 31/05/2017.
+//  Copyright © 2017 Designated Nerd Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertController (Convenience)
+
++ (void) showSimpleAlertWithTitle: (NSString *) title andMessage: (NSString *) message;
+
+@end
