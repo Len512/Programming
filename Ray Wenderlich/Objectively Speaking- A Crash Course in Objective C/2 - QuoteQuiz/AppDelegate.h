a12ac9e511fc987b0f56109b5245a42ab06a3daf
Len512 <echatzak@gmail.com>
2017-06-01 10:02:55 +0200

    Create AppDelegate.h

---
+++ Ray Wenderlich/Objectively Speaking- A Crash Course in Objective C/2 - QuoteQuiz/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  QuoteQuiz
+//
+//  Created by Len512 on 31/05/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
